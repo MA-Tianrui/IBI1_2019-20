9d38c73282949e0a66598ebe955996ad8b3163ce
MTR <909517489@qq.com>
2020-03-11 11:55:37 +0800

    Add some comments in mysterycode.py

--- Practical5/mystery_code.py
+++ Practical5/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer: 
+# Answer: The code output a random prime number (or 1) from 1 to 100
 
 # Import libraries
 # randint allows drawing a random number, 
@@ -13,12 +13,17 @@ from math import ceil
 p=False
 while p==False:
     p=True
+#draw a number between 1 and 100
     n = randint(1,100)
+#compute the square root of n, and get the next higher integer of the square root
     u = ceil(n**(0.5))
+#check whether n can be divided by integer from 2 to the particular integer larger than its square
     for i in range(2,u+1):
         if n%i == 0:
+#if n can be divided, repeat the above step, draw a new n
             p=False
 
 
      
+#if n cannot be divided, n is a prime number (or 1), print n
 print(n)
