aec6190963417092f3cabe982cc0cfd698e5e51a
MTR <909517489@qq.com>
2020-03-12 15:20:52 +0800

    Revise powersof2.py

--- Practical5/powersof2.py
+++ Practical5/powersof2.py
@@ -2,13 +2,15 @@
 N = float(input("Enter a positive integer N:"))
 #print the start of sentence
 print(str(N), "is", end = " ")
-#loop
+#a loop from 12 to 0, assume that N < 8192
 for i in range(12,-1,-1):
 #if the integer larger than a power of 2, minus the power of 2
     if N >= 2**i:
         N = N - 2**i
 #print every power of 2
-        if N == 0:
-            print("2**", str(i))
+#change float into str
+        power = str(i)
+        if N == 0: 
+            print("2**", power, sep="")
         else:
-            print("2**", str(i), "+", end = " ")
+            print("2**", power, " +", end = " ", sep="")
