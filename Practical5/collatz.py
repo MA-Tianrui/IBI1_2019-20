fcbbbc9cb19dcf28bdbc40af561e141e699a505f
MTR <909517489@qq.com>
2020-03-12 16:15:28 +0800

    Polish the comment in collatz.py

--- Practical5/collatz.py
+++ Practical5/collatz.py
@@ -1,10 +1,11 @@
 #take a positive integer n
 N = float(input("Enter a positive integer:"))
+#check if it is a positive integer
 if N <= 0 or N%1 != 0:
     print("it is not a positive integer.")
 else:
     print(N)
-#repeat
+#repeat until 1 occurs
     while N != 1:
 #check whether it is an odd or an even, then compute
         if N %2 == 0:
@@ -12,5 +13,5 @@ else:
         else:
             N = N*3+1
         print(N)
-#stop when it is 1
+#stop when 1 occurs
     print("We stop at 1.")
