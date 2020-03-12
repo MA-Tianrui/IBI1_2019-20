72880aaee88e6311de027754c01eb72ccf995d9d
MTR <909517489@qq.com>
2020-03-12 16:15:46 +0800

    Polish the comments in powersof2.py

--- Practical5/powersof2.py
+++ Practical5/powersof2.py
@@ -7,9 +7,10 @@ for i in range(12,-1,-1):
 #if the integer larger than a power of 2, minus the power of 2
     if N >= 2**i:
         N = N - 2**i
-#print every power of 2
 #change float into str
         power = str(i)
+#print every power of 2
+#when N=0, we do not need a "+" behind the powers of 2
         if N == 0: 
             print("2**", power, sep="")
         else:
