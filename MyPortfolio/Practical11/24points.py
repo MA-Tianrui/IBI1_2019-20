a464d7628782712716fdab668d820394909a3b09
MTR <909517489@qq.com>
2020-05-13 20:49:32 +0800

    Revise the code (big revision)

--- MyPortfolio/Practical11/24points.py
+++ MyPortfolio/Practical11/24points.py
@@ -1,43 +1,87 @@
-#data = [1, 2, 3, 4, 5, 6]
-#def combine(data, l):
-#    result = []
-#    tmp = [0]*l
-#    length = len(data)
-#    def next_num(li=0, ni=0):
-#        if ni == l:
-#            result.append(copy.copy(tmp))
-#            return
-#        for lj in range(li,length):
-#            tmp[ni] = data[lj]
-#            next_num(lj+1, ni+1)
-#    next_num()
-#    print(result)
-#    return result
-# 
-# 
-#combine(data,3)
+import sys
 
-def PointGame(group):
+# input a group of numbers and check if they fulfill the criteria
+ori = input("Please input numbers to compute 24 (use ',' to divide them): ")
+
+num = []
+num_str = ori.split(',')
+for i in num_str:
+    j = float(i)
+    num.append(j)
+    if j%1 != 0 or j > 23 or j < 1:
+        print('The input number must be integers from 1 to 23')
+        sys.exit(0)
+
+
+# get all the sitution and put them into one list
+def cal(l):
+   rl = []
+   for i in list(range(l-1)):
+      for j in list(range(i+1,l)):
+          for k in range(6):
+             rl.append([i, j, k])
+   return rl
+   
+
+sc = [[], []]
+for i in range(2, len(num) + 1):
+   sc.append(cal(i))
+
+
+sci = []
+for i in range(len(num)):
+   sci.append(sc[len(num) - i])
+
+
+allSit = []
+def select_copy(l):
+   l_copy = l.copy()
+   if len(l_copy) == len(sci) - 1:
+      allSit.append(l_copy)
+   for i in sci[len(l)]:
+      l.append(i)
+      select_copy(l)
+      l.pop()
+   
+select_copy([])
+
+
+# try all the calculation way between two number
+def op(i, j, k, l):
+    if k == 0:
+        value = l[i] + l[j]
+    elif k == 1:
+        value = l[i] - l[j]
+    elif k == 2:
+        value = l[j] - l[i]
+    elif k == 3:
+        value = l[i] * l[j]
+    elif k == 4 and l[j] != 0:
+        value = l[i] / l[j]
+    elif k == 5 and l[i] != 0:
+        value = l[j] / l[i]
+    else:
+        value = 0
+    if value == 24:
+        return 1
+    else:
+        l[i] = value
+        l.pop(j)
+        return 0
     
+# find if 24 can be got in every situation and calculate the repeat times
+get24 = False
+n = 0
+for i in allSit:
+   num_copy=num.copy()
+   for j in i:
+       if op(j[0], j[1], j[2], num_copy) == 1 and get24 == False:
+          print("Yes")
+          get24 = True
+       n += 1
 
-#target每组要输出的元素个数，step游标
-def combine(data, step, select_data, target_num):
-    #如果已经凑齐一组完成的组合，输出当前组
-    if len(select_data) == target_num:
-        points(select_data)
-        return
-    #如果游标超过数组长，说明后续元素都被遍历完成，跳转到上一个元素的循环
-    if step >= len(data):
-        return
-    #把选中的元素加入临时输出列表中，n个一组作为输出
-    select_data.append(data[step])
-    #递归调整最后一位
-    combine(data, step + 1, select_data, target_num)
-    #构建一个新组合，首先要删掉上次输出组中药排除的元素
-    select_data.pop()
-    #递归向上调整一位元素                         
-    combine(data, step + 1, select_data, target_num)
- 
+if get24 == False:
+    print("No")
 
-data = [1, 2, 3, 4, 5, 6]
-combine(data, 0, [], 3)
+# this number of times is the worst situation
+print('Times:', n)
